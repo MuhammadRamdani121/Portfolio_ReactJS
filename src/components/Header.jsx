1fda0861a4d4a4fa42122339ccd07a2c8bed2e7e
MuhammadRamdani121 <muhamadramdani121.mr60@gmail.com>
2026-04-04 15:54:21 +0700

    Menambahkan Navbar Pada Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,57 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import fotoDiri from "../assets/Foto_Diri.jpg";
+
 export default function Header() {
+  const [open, setOpen] = useState(false);
+
   return (
-    <header className="flex justify-between px-20 bg-red-200 text-lg">
-      <h1>R</h1>
-      <nav className="flex gap-4 text-lg">
-        <a href="">Home</a>
-        <a href="">About</a>
-        <a href="">Project</a>
-        <a href="">Contact</a>
+    <header className="bg-red-200  text-xl">
+      <div className="flex justify-between items-center px-6">
+        <img
+          className={`${open ? "hidden" : "block"} md:block w-12 h-12 rounded-full`}
+          src={fotoDiri}
+          alt="foto"
+        />
+
+        {/* MENU DESKTOP */}
+        <nav className="hidden md:flex gap-6">
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/project">Project</Link>
+          <Link to="/contact">Contact</Link>
+        </nav>
+
+        {/* HAMBURGER */}
+        <button
+          className="ml-auto block text-2xl md:hidden"
+          onClick={() => setOpen(!open)}
+        >
+          {open ? "X" : "☰"}
+        </button>
+      </div>
+
+      {/* MOBILE MENU */}
+      <nav
+        className={`md:hidden px-6 pb-4 transition-all duration-300 ${
+          open ? "block" : "hidden"
+        }`}
+      >
+        {/* tombol X kanan */}
+        <div className="flex flex-col gap-4 items-center ">
+          <Link to="/" onClick={() => setOpen(false)}>
+            Home
+          </Link>
+          <Link to="/about" onClick={() => setOpen(false)}>
+            About
+          </Link>
+          <Link to="/project" onClick={() => setOpen(false)}>
+            Project
+          </Link>
+          <Link to="/contact" onClick={() => setOpen(false)}>
+            Contact
+          </Link>
+        </div>
       </nav>
     </header>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./app";
 import "./index.css";
-import Header from "./components/header";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <Header />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
 );
