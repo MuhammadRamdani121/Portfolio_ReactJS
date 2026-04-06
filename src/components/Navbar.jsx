db1835a8893991fe0cb20d3e8e2f1011a30b6a53
MuhammadRamdani121 <muhamadramdani121.mr60@gmail.com>
2026-04-06 21:09:48 +0700

    Menambahkan Versi Navbar Sendiri

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,11 +5,11 @@ export default function Navbar() {
   const [open, setOpen] = useState(false);
   return (
     <>
-      <header className="flex justify-between item-center">
+      <header className="flex justify-between item-center fixed w-full bg-amber-200">
         {/* logo */}
         <h1 className="text-4xl font-bold">adasssss</h1>
         {/* desktop menu */}
-        <nav className="flex flex-row gap-10">
+        <nav className="hidden md:flex md:items-center md:justify-center gap-10 w-1/3 ">
           <a href="">asd</a>
           <a href="">ss</a>
           <a href="">bbb</a>
@@ -18,16 +18,24 @@ export default function Navbar() {
         </nav>
 
         {/* Button Hamburger */}
-        <button onClick={() => setOpen(!open)}>
-          {open ? <X size={28} /> : <Menu size={28} />}
+        <button
+          className="text-4xl px-6 md:hidden"
+          onClick={() => setOpen(!open)}
+        >
+          {open ? "X" : "☰"}
+          {/* {open ? <X size={28} /> : <Menu size={28} />} */}
         </button>
 
         {/* hamburger menu */}
         {open && (
-          <div>
-            <nav>
-              <a href="">asd</a>
-              <a href="">ss</a>
+          <div className="flex flex-row bg-red-300 fixed w-full top-10">
+            <nav className="flex flex-col items-center justify-center gap-10 h-auto w-full bg-amber-900">
+              <a href="" className="bg-white">
+                a
+              </a>
+              <a href="" className="bg-white">
+                b
+              </a>
               <a href="">bbb</a>
               <a href="">ccc</a>
               <a href="">dd</a>
